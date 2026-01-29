11310d02f1cf1ac83b58a3da7eb0e609cb21de9e
omar10331 <98680999+omar10331@users.noreply.github.com>
2026-01-29 04:02:47 +0200

    Create 01-Variables-and-Casting.cpp (#25)

---
+++ Team_Workspace/Omar_Gasser/01-Variables-and-Casting/01-Variables-and-Casting.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int time_microseconds = 5882;
+    double distance_centimeters = static_cast<double>(time_microseconds) / 58;
+    double distance_meters = (static_cast<double>(distance_cm)) / 100;
+    cout << distance_meters;
+    return 0;
+}
