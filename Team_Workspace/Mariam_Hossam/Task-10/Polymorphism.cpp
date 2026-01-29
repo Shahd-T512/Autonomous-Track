9b3a39b7cbdd21bb9de6e7f1683a9c2e56f2d0be
Mariam-Hossam321 <mh2905@fayoum.edu.eg>
2026-01-30 00:48:04 +0200

    Final Submission Tasks 1-10 (#39)

---
+++ Team_Workspace/Mariam_Hossam/Task-10/Polymorphism.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class PaymentMethod {
+    public:
+        virtual void pay(double amount) {
+            cout << "Generic Payment processing..." << endl;
+        }
+};
+
+class CreditCard : public PaymentMethod {
+    public:
+        void pay(double amount) override {
+            cout << "Processing Credit Card payment of $" << amount << endl;
+        }
+};
+
+class Cash : public PaymentMethod {
+    public:
+        void pay(double amount) override {
+            cout << "Accepting Cash payment of $" << amount << endl;
+        }
+};
+
+int main() {
+    CreditCard myCard;
+
+    PaymentMethod& paymentSystem = myCard;
+
+    paymentSystem.pay(15.50);
+
+    return 0;
+}
---
+++ Team_Workspace/Mariam_Hossam/Task-8/Constructors & Destructors.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Logger {
+public:
+    Logger() {
+        cout << "--- LOG STARTED ---" << endl;
+    }
+
+    ~Logger() {
+        cout << "--- LOG SAVED & CLOSED ---" << endl;
+    }
+};
+
+int main() {
+    cout << "Step 1" << endl;
+
+    {
+        Logger my_log;
+        cout << "Step 2 (Inside Scope)" << endl;
+    } 
+    
+    cout << "Step 3 (Outside Scope)" << endl;
+
+    return 0;
+}
