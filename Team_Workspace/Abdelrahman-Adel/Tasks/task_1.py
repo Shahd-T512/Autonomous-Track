b836c397dacc97f55895f6f5242ad48b5b80b226
Abdo803423 <adlabdo888@gmail.com>
2026-01-31 13:56:00 +0200

    Done (#52)

--- Team_Workspace/Abdelrahman-Adel/Tasks/task_1.py
+++ Team_Workspace/Abdelrahman-Adel/Tasks/task_1.py
@@ -1,9 +1,4 @@
-num = [0, 0.1, -1]
-
-for n in num:
-    if n < 0:
-        print("Error")
-    elif n == 0:  
-        print("Blind spot")
-    else:
-        print("valid Range")
+time_microsecounds = 5882
+Distace_Cm = float(time_microsecounds) / 58
+Distace_m = float(Distace_Cm) / 100
+print("The Distance is "+ str(Distace_m) +" Meter")
